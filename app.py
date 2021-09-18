d0bf9daa294b68b62c7e2a7cfdfd07c8593f4988
godifay <godifay@yahoo.com>
2021-09-17 22:30:53 -0700

    commit -m 'second commit'

--- app.py
+++ app.py
@@ -1,9 +1,48 @@
 import streamlit as st
-st run : app.py
-import streamlit as st
 import numpy as np
 import pandas as pd
 st.write("# Python Web App")
 st.write("Hi, this is our first web app in Python! :sunglasses:")
 df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
 st.write(df)
+input_value = st.text_input("Enter a Message")
+if st.button("Display Message"):
+    st.write(input_value)
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)    
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
+option = st.selectbox(
+    'Which number do you like best?',
+     df['col1'])
+
+'You selected: ', option    
+left_column, right_column = st.columns(2)
+pressed = left_column.button('Press me?')
+if pressed:
+  right_column.write("Woohoo!")
+
+expander = st.expander("FAQ")
+expander.write("Here you could put in some really, really long explanations...")
+import time
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
